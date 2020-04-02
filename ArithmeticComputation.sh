042106ed05fa994fd0709e9f6bb44ccba8202a42
samir0786 <samirsiescom@gmail.com>
2020-04-02 17:50:57 +0300

    ADDED: Store Risult in Array

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash/ -x
 read -p "Enter three number: " a b c
+count=0
+declare -A results
 
 function operation4() {
    echo $(($(($a%$b)) + $c))
@@ -17,9 +19,15 @@ function operation1() {
    echo $(($(($a+$b)) * $c))
 }
 
-declare -A results
 results["operation1"]=$(operation1 $a $b $c)
 results["operation2"]=$(operation2 $a $b $c)
 results["operation3"]=$(operation3 $a $b $c)
 results["operation4"]=$(operation4 $a $b $c)
-echo ${results[@]}
+#echo ${results[@]}
+
+while [[ $count -ne ${#results[@]} ]]
+do
+	arr[$count]=${results[operation"$(( count+1 ))"]}
+	count=$(( count+1 ))
+done
+echo ${arr[@]}
