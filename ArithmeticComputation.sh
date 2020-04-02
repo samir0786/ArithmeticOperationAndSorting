8455e21a3633c13af85c349a427603e91bf5588b
samir0786 <samirsiescom@gmail.com>
2020-04-02 16:32:43 +0300

    ADDED: Function to Compute a%b+c

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,8 +1,12 @@
 #!/bin/bash/ -x
 read -p "Enter three number: " a b c
 
+function operation4() {
+   echo $(($(($a%$b)) + $c))
+}
+
 function operation3() {
-	echo $(($(($a+$b)) / $3))
+	echo $(($(($a+$b)) / $c))
 }
 
 function operation2() {
@@ -12,5 +16,5 @@ function operation2() {
 function operation1() {
    echo $(($(($1+$2)) * $3))
 }
-operation3 $a $b $c
+operation4 $a $b $c
 
